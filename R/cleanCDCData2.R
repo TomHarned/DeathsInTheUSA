6e66f4321d4730302149f66fe92273c881011a08
TomHarned <thomas.harned@gmail.com>
2018-03-13 20:59:53 -0400

    Initial publication, updated import and export functions

--- R/cleanCDCData2.R
+++ R/cleanCDCData2.R
@@ -1,23 +1,53 @@
 
-library(tidyverse)
-library
-proj_path <- "/Users/tomharned/DeathsInTheUSA"
+require(tidyverse)
 
-importCDC <- function(file_path, out_dir){
-  #insert functions here
+
+importCDC <- function(){
+  list_of_files <- list.files('./Input Data/', include.dirs = F, pattern = "\\.txt$")
+  list_of_dfs <- purrr::map(.x = paste0('Input Data/', list_of_files), .f = ~read_tsv(.x))
+  names(list_of_dfs) <- c("ByGender", "ByState", "ByRace")
+  list_of_dfs
+  list2env(list_of_dfs, .GlobalEnv)
 }
 
+
 cleanCDC <- function(df, ...){
-  names(df) <- gsub(" ", "_", names(df))
-    df %>%
+  names(df) <- gsub(" |-", "_", names(df))
+    
+  df <- df %>%
     filter(is.na(Notes) == T) %>% 
     select(-Notes) %>%
-    mutate(Population = as.numeric(Population),
-           # Create a seperate Race Category for Hispanics
-           Race = ifelse(Hispanic_Origin == "Hispanic or Latino", "Hispanic", Race)) %>%
+    mutate(Population = as.numeric(Population)) 
+    
+    if("Race" %in% names(df)){
+     df$Race <-  ifelse(df$Hispanic_Origin == "Hispanic or Latino", 
+                        "Hispanic or Latino", df$Race)
+    } 
+    
+    df %>%
     group_by_(.dots = lazyeval::lazy_dots(...)) %>% 
     summarize(Deaths = sum(Deaths, na.rm=T),
               Population = sum(Population, na.rm=T))
 }
 
+outputCleanFile <- function(df, out_file_name){
+                      write.csv(df, 
+                      file = paste0('./Input Data/derived_data/', out_file_name, '.csv'),
+                      row.names = F)
+}
 
+##### Execute Functions #####
+importCDC()
+
+ByGender %>% 
+  cleanCDC(Year, Gender, Race) %>% outputCleanFile(out_file_name = "ByGender")
+
+ 
+ByRace %>% 
+  cleanCDC(Year, Ten_Year_Age_Groups, Race) %>% 
+  outputCleanFile(out_file_name = "ByRace")
+
+
+ByState %>% 
+  cleanCDC(State, `2013_Urbanization`, Year) %>% 
+  outputCleanFile(out_file_name = "ByState")
